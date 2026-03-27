8b2e3cb1c25d9609bccf2f9b5179f95d57f11299
Yosed Camilo Sanchez P. <Camilos0704@gmail.com>
2026-03-27 14:06:09 -0500

    desactivar seguridad para poder acceder a rutas

--- ProyectoUTurismo/src/app/guards/checklogin-guard.ts
+++ ProyectoUTurismo/src/app/guards/checklogin-guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const checkloginGuard: CanActivateFn = (route, state) => {
-  const router = inject(Router);
+/**  const router = inject(Router);
   const token = localStorage.getItem('access');
   if (token) {
     return true;
@@ -10,4 +10,6 @@ export const checkloginGuard: CanActivateFn = (route, state) => {
     router.navigate(['/login']);
     return false;
   }
+  */
+ return true
 };
--- ProyectoUTurismo/src/app/guards/login.guard.ts
+++ ProyectoUTurismo/src/app/guards/login.guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const loginGuard: CanActivateFn = (route, state) => {
-  const router = inject(Router);
+ /**  const router = inject(Router);
   const token = localStorage.getItem('access');
   
   if (token) {
@@ -10,5 +10,6 @@ export const loginGuard: CanActivateFn = (route, state) => {
     return false;
   } else {
     return true;
-  }
+  }*/
+ return true
 };
